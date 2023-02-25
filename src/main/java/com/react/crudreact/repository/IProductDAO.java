53d5af3b8926b337bea7fcac05390ef4962648b3
Adrisher <adrian.guerrero.est@tecazuay.edu.ec>
2023-02-25 17:40:24 -0500

    BackMongReactV2

--- src/main/java/com/react/crudreact/repository/IProductDAO.java
+++ src/main/java/com/react/crudreact/repository/IProductDAO.java
@@ -2,6 +2,8 @@ package com.react.crudreact.repository;
 
 import com.react.crudreact.model.ProductDTO;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface IProductDAO extends MongoRepository<ProductDTO, String> {
 }
